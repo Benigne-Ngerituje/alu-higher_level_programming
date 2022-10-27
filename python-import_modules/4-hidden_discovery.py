425e8a92916701cc169c621969f829eec2b68cde
Benigne-Ngerituje <b.ngerituje@alustudent.com>
2022-10-27 11:30:18 +0200

    hidden

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-import hidden_4
-if __name__ == '__main__':
-    names = dir(hidden_4)
-    names.sort()
-    for name in names:
-        if name[:2] != "__":
-            print(name)
+if __name__ == "__main__":
+    import hidden_4
+    for i in dir(hidden_4):
+        if not i.startswith("__"):
+            print("{:s}".format(i))
             
