4b9afdb231e6472795036c8be1195aff90294212
Benigne-Ngerituje <b.ngerituje@alustudent.com>
2022-11-02 22:51:35 +0200

    be ready

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 1,2,3,4,5  #integers
-my_list = [1,2,3,4]
-print_list_integer = my_list.format()
-print(print_list_integer)
-print(my_list)
+
+def print_list_integer(my_list=[]):
+    for integer in my_list:
+        print("{:d}".format(integer))
+        
+
