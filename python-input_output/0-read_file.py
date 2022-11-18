9f01da27c1abf98ede3f1aa2e51d6057e7b1e1ce
Benigne-Ngerituje <b.ngerituje@alustudent.com>
2022-11-18 12:04:54 +0200

    input

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,8 +1,16 @@
 #!/usr/bin/python3
-"""This module define a text file-reading function."""
+# function that reads a text file (UTF8),
+# and prints it to stdout
+"""
+    define a function 'read_file'
+"""
 
 
 def read_file(filename=""):
-    """Print the contents"""
-    with open(filename, encoding="utf-8") as f:
-        print(f.read(), end="")
+    """
+        reads a text file & prints it to stdout
+    """
+
+    with open(filename, encoding='utf-8') as f:
+        for line in f:
+            print(line, end="")
