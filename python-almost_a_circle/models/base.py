331ef3970d552a76be09c93a7d0879cf84f29967
Benigne-Ngerituje <b.ngerituje@alustudent.com>
2022-12-12 22:26:23 +0200

    try

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -8,10 +8,11 @@ class Base:
 
     __nb_objects = 0 #variable 
 
-    def __init__(self, id=None): #function with id as parameter
+    def __init__(self, id=None): 
+        
         """Initialize a new id"""
-        if id is not None: #conditional to check if ID is equal to none
-            self.id = id #make self.id equal to id
-        else: #if id is none
-            Base.__nb_objects += 1 #increment the value of base
-            self.id = Base.__nb_objects #equate base to self.id
+        if id is not None: 
+            self.id = id 
+        else: 
+            Base.__nb_objects += 1 
+            self.id = Base.__nb_objects 
