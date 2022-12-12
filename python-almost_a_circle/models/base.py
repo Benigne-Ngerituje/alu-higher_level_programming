8796b1c36001e9f23b90daf5969068a8696273e2
Benigne-Ngerituje <b.ngerituje@alustudent.com>
2022-12-12 22:22:33 +0200

    try

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,11 +1,21 @@
+
+
 #!/usr/bin/python3
+"""A base class"""
 
-class Base: #the class base
-    __nb_objects = 0 #variable for _nb objects as instructed
+import json
+import turtle
+import csv
 
-    def __init__(self, id=None): #function with id as parameter
-        if id is not None: #conditional to check if ID is equal to none
-            self.id = id #make self.id equal to id
-        else: #if id is none
-            Base.__nb_objects += 1 #increment the value of base
-            self.id = Base.__nb_objects #equate base to self.id
+
+class Base:
+    """A base class"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Initialize a new id"""
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
