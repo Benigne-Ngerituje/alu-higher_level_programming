d2b6fecec91f2b4203479afa2f646295205a290c
Benigne-Ngerituje <b.ngerituje@alustudent.com>
2022-12-12 21:58:55 +0200

    init3

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -12,7 +12,7 @@
 #             Base.__nb_objects += 1
 #             self.id = Base.__nb_objects
 
-class Base:
+class Base: #the class base
     """Base class for managing object ids.
 
     This class provides a consistent way to manage the id attribute of objects
@@ -26,7 +26,7 @@ class Base:
 
     __nb_objects = 0
 
-    def __init__(self, id=None):
+    def __init__(self, id=None): #function with id as parameter
         """Initializes the object with a unique id value.
 
         Args:
@@ -37,8 +37,8 @@ class Base:
         Raises:
             TypeError: If the id argument is provided but is not an integer.
         """
-        if id is not None:
-            self.id = id
-        else:
-            Base.__nb_objects += 1
-            self.id = Base.__nb_objects
+        if id is not None: #conditional to check if ID is equal to none
+            self.id = id #make self.id equal to id
+        else: #if id is none
+            Base.__nb_objects += 1 #increment the value of base
+            self.id = Base.__nb_objects #equate base to self.id
