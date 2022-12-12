4b56cea89ecb6aab5e8531da6161258f2ad5ab1b
Benigne-Ngerituje <b.ngerituje@alustudent.com>
2022-12-12 22:41:09 +0200

    try

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -2,24 +2,14 @@
 
 """A base class"""
 
-
-class Base: 
-
-
+class Base:
     """A base class for the file"""
-
-
-
     __nb_objects = 0 
 
-
-
-    def __init__(self,id=None): 
-
-        """Initializing a new id"""
-
-        if id is not None: 
-            self.id = id 
-        else: 
-            Base.__nb_objects += 1 
-            self.id = Base.__nb_objects 
+def __init__(self, id=None): 
+    """Initializing a new id"""
+    if id is not None: 
+        self.id = id 
+    else: 
+        Base.__nb_objects += 1 
+        self.id = Base.__nb_objects
