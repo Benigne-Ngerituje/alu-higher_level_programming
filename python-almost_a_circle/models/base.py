367a70599bdac21880483e84a3f3fcc6b15fd74b
Benigne-Ngerituje <b.ngerituje@alustudent.com>
2022-12-12 22:04:19 +0200

    init4

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,42 +1,9 @@
-#!/usr/bin/python3 #beeded as instructed"
-# models/base.py
-
-
-# class Base:
-#     __nb_objects = 0
-
-#     def __init__(self, id=None):
-#         if id is not None:
-#             self.id = id
-#         else:
-#             Base.__nb_objects += 1
-#             self.id = Base.__nb_objects
+#!/usr/bin/python3 
 
 class Base: #the class base
-    """Base class for managing object ids.
-
-    This class provides a consistent way to manage the id attribute of objects
-    in a project. The id attribute is initialized to a unique integer value
-    when an object is created, unless a specific id value is provided.
-
-    Attributes:
-        __nb_objects (int): Private class attribute used to keep track of the
-            number of objects created.
-    """
-
     __nb_objects = 0 #variable for _nb objects as instructed
 
     def __init__(self, id=None): #function with id as parameter
-        """Initializes the object with a unique id value.
-
-        Args:
-            id (int, optional): If provided, this value will be used as the
-                id of the object. If not provided, a unique id value will be
-                generated for the object.
-
-        Raises:
-            TypeError: If the id argument is provided but is not an integer.
-        """
         if id is not None: #conditional to check if ID is equal to none
             self.id = id #make self.id equal to id
         else: #if id is none
