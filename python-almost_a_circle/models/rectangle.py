c3df5b1dcccf14216a4cf15fe4b9311a66d69e18
Benigne-Ngerituje <b.ngerituje@alustudent.com>
2022-12-14 08:20:13 +0200

    rectangle

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,8 +1,17 @@
 #!/usr/bin/python3
+
+
+"""A rectangle class"""
+
+
 class Rectangle(Base):
+
     """A class for representing rectangles."""
 
     def __init__(self, width, height, x=0, y=0, id=None):
+
+        """ initializing a new width, height, x value, y value and id """
+        
         super().__init__(id)
         self.__width = width
         self.__height = height
@@ -10,24 +19,36 @@ class Rectangle(Base):
         self.__y = y
 
     def width(self, value=None):
+
+        """ assigning a value to width """
+
         if value is None:
             return self.__width
         else:
             self.__width = value
 
     def height(self, value=None):
+
+        """ assigning a value to height """
+
         if value is None:
             return self.__height
         else:
             self.__height = value
 
     def x(self, value=None):
+
+        """ assigning a value to x """
+
         if value is None:
             return self.__x
         else:
             self.__x = value
 
     def y(self, value=None):
+
+        """ assigning a value to y """
+        
         if value is None:
             return self.__y
         else:
