34376578492d02601f3de92e9358019f6ad09451
Benigne-Ngerituje <b.ngerituje@alustudent.com>
2022-12-14 08:38:17 +0200

    rectangle

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -6,52 +6,77 @@ from models.base import Base
 
 
 class Rectangle(Base):
-
-    """A class for representing rectangles."""
+    """Rectangle class"""
 
     def __init__(self, width, height, x=0, y=0, id=None):
-
-        """ initializing a new width, height, x value, y value and id """
-        
         super().__init__(id)
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        """Getting the Width"""
+
+        return self.__width
 
     @width.setter
-    def width(self, value=None):
+    def width(self, value):
+        """Setting the width"""
 
-        """ assigning a value to width """
+        if type(value) != int:
+            raise TypeError("width must be an integer")
 
-        if value is None:
-            return self.__width
-        else:
-            self.__width = value
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """height of a rectangle"""
+
+        return self.__height
 
     @height.setter
-    def height(self, value=None):
+    def height(self, value):
+        """Height"""
 
-        """ assigning a value to height """
+        if type(value) != int:
+            raise TypeError("height must be an integer")
 
-        if value is None:
-            return self.__height
-        else:
-            self.__height = value
+        if value <= 0:
+            raise ValueError("height must be > 0")
 
-    def x(self, value=None):
+        self.__height = value
 
-        """ assigning a value to x """
+    @property
+    def x(self):
+        """x"""
+        return self.__x
 
-        if value is None:
-            return self.__x
-        else:
-            self.__x = value
+    @x.setter
+    def x(self, value):
+        """x"""
+        if type(value) != int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
 
-    def y(self, value=None):
+    @property
+    def y(self):
+        """return y"""
+        return self.__y
 
-        """ assigning a value to y """
-        if value is None:
-            return self.__y
-        else:
-            self.__y = value
+    @y.setter
+    def y(self, value):
+        """Set the value of y"""
+
+        if type(value) != int:
+            raise TypeError("y must be an integer")
+
+        if value < 0:
+            raise ValueError("y must be >= 0")
+
+        self.__y = value
