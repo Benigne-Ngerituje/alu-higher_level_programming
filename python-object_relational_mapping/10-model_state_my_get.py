aec749d0a5570a1b06fb3740922b1e6a00390706
Benigne-Ngerituje <b.ngerituje@alustudent.com>
2022-12-16 23:50:16 +0200

    execute

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -1,30 +1,31 @@
 #!/usr/bin/python3
-# script that prints the 'State' object,
-# with the 'name' passed as argument from the database 'hbtn_0e_6_usa'
 """
-   import 'sys' & 'MySQLdb'
+return state id given state name; SQL injection free
+parameters given to script: username, password, database, state name to match
 """
-import sys
+
+from sys import argv
+from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import State
+
 
 if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
+
+    # make engine for database
+    user = argv[1]
+    passwd = argv[2]
+    db = argv[3]
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(user, passwd, db), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    found = False
-    for state in session.query(State):
-        if state.name == sys.argv[4]:
-            print("{}".format(state.id))
-            found = True
-            break
-    if found is False:
-        print("Not found")
+    # query python instance in database state id given state name
+    state = session.query(State).filter_by(name=argv[4]).first()
+    if state:
+        print("{:d}".format(state.id))
     else:
-        print(f'{state.id}')
-
+        print("Not found")
     session.close()
