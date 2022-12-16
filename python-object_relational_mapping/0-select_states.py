c4389cc21dc61daf6cb7de85bc41b7923c99c06c
Benigne-Ngerituje <b.ngerituje@alustudent.com>
2022-12-16 21:00:37 +0200

    map

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,28 +1,14 @@
 #!/usr/bin/python3
 """
-    Create a script that lists all states from a database hbtn_0e_0_usa
+return states starting with 'N'
+parameters given to script: username, password, database
 """
 
-import sys
 import MySQLdb
-
+from sys import argv
 
 if __name__ == "__main__":
-    db_conn = MySQLdb.connect(
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3],
-        host='localhost',
-        port=3306
-    )
-
-    cur = db_conn.cursor()
-
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
-    states = cur.fetchall()
-
-    for state in states:
-        print(state)
-
-    cur.close()
-    db_conn.close()
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM `states`")
+    [print(state) for state in c.fetchall()]
