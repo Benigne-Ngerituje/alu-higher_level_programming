7c8509047b9ab9c9b9274953a32261c1f696e4bf
Benigne-Ngerituje <b.ngerituje@alustudent.com>
2022-12-16 23:30:24 +0200

    execute

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,15 +1,7 @@
 #!/usr/bin/python3
 """
-This script retrieves all state objects from a MySQL database and prints their
-ID and name fields to the console.
-
-To run the script, provide the following parameters at the command line:
- - username: the username to use when connecting to the MySQL database
- - password: the password to use when connecting to the MySQL database
- - database: the name of the database to connect to
-
-The script assumes that the MySQL connector library and SQLAlchemy are
-installed, and that the 'model_state' module is available.
+return all state objects from database via python
+parameters given to script: username, password, database
 """
 
 from sys import argv
@@ -17,30 +9,21 @@ from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-def main():
-    """
-    Connect to the MySQL database, retrieve all state objects from it, and
-    print their ID and name fields.
-    """
-    # Create a connection to the MySQL database
+
+if __name__ == "__main__":
+
+    # make engine for database
     user = argv[1]
     passwd = argv[2]
     db = argv[3]
-    engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost/{}'.format(user, passwd, db),
-        pool_pre_ping=True
-    )
-
-    # Create a session to the database
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(user, passwd, db), pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    # Retrieve all state objects from the database and print their ID and name
+    # query python instances in database
     for instance in session.query(State).order_by(State.id):
         print("{:d}: {:s}".format(instance.id, instance.name))
 
-    # Close the session to clean up resources
     session.close()
-
-if __name__ == "__main__":
-    main()
+    
