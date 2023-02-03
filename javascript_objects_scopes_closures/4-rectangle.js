2a2b3f47ab66ab35f21d4eb816fe0529884cec9f
Benigne-Ngerituje <109472115+Benigne-Ngerituje@users.noreply.github.com>
2023-02-03 22:51:27 +0000

    scopes in Javascript

--- javascript_objects_scopes_closures/4-rectangle.js
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -11,16 +11,16 @@ module.exports = class Rectangle {
     for (let i = 0; i < this.height; i++) {
       console.log('X'.repeat(this.width));
     }
-}
-  
-    rotate () {
-      const tempWidth = this.width;
-      this.width = this.height;
-      this.height = tempWidth;
-    }
-  
-    double () {
-      this.width *= 2;
-      this.height *= 2;
-    }
+  }
+
+  rotate () {
+    const tempWidth = this.width;
+    this.width = this.height;
+    this.height = tempWidth;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
   };
