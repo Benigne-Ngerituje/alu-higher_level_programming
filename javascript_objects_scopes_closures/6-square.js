3a1657e6f09e471b5e562fa06e6b4937d4e94d70
Benigne-Ngerituje <109472115+Benigne-Ngerituje@users.noreply.github.com>
2023-02-03 23:03:50 +0000

    scopes in Javascript

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -2,9 +2,9 @@
 
 module.exports = class Square extends require('./5-square') {
     charPrint (c) {
-      const char = c || 'X';
-      for (let i = 0; i < this.height; i++) {
-        console.log(char.repeat(this.width));
+    const char = c || 'X';
+    for (let i = 0; i < this.height; i++) {
+      console.log(char.repeat(this.width));
       }
     }
   };
